03b38565036d1b38ae3df1c4442622a7a5a0b306
archi-Doc <archidoc422@gmail.com>
2021-09-18 21:34:31 +0900

    .

--- Benchmark/#ps1/#Run.ps1
+++
@@ -1,9 +0,0 @@
-# Set-ExecutionPolicy RemoteSigned
-
-# Run
-docker run -it archidoc422/arc-crypto-bench
-
-Write-Output ""
-
-Write-Output "" "Press any key to exit."
-Read-Host
--- Benchmark/#ps1/2/#Run.ps1
+++
@@ -1,8 +0,0 @@
-# Set-ExecutionPolicy RemoteSigned
-
-# Run
-docker run -it archidoc422/arc-crypto-bench
-Write-Output ""
-
-Write-Output "" "Press any key to exit."
-Read-Host
