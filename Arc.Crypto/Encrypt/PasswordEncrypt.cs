5c2211a3588c7a82ef85f14345417754ddcb69bb
archi-Doc <archidoc422@gmail.com>
2021-11-09 18:00:04 +0900

    .

--- Arc.Crypto/Encrypt/PasswordEncrypt.cs
+++ Arc.Crypto/Encrypt/PasswordEncrypt.cs
@@ -2,6 +2,8 @@
 
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
+using System.Diagnostics.CodeAnalysis;
 using System.Linq;
 using System.Security.Cryptography;
 using System.Text;
@@ -18,56 +20,88 @@ public static class PasswordEncrypt
 {
     public const int DefaultStretchingCount = 32;
     public const int SaltLength = 8;
+    public const PaddingMode DefaultPaddingMode = PaddingMode.PKCS7;
 
     public static byte[] Encrypt(byte[] data, string password) => Encrypt(data, Encoding.UTF8.GetBytes(password));
 
     public static byte[] Encrypt(byte[] data, byte[] password)
     {
-        // Hash: Sha3_384
-        var hash = new Sha3_384();
-        var hashLength = hash.HashBytes;
+        // Salt: Random[SaltLength]
+        var salt = RandomNumberGenerator.GetBytes(SaltLength);
+
+        // Hash: Sha3_384 => Key(32) + IV(16)
+        var keyIV = GetKeyIV(salt, password);
 
         // Checksum: FarmHash64
         var checksum = FarmHash.Hash64(data);
 
-        // Salt: Random[SaltLength]
-        var salt = RandomNumberGenerator.GetBytes(SaltLength);
-
-        // SHA3-384(Salt(8) + Password(n) + Previous Hash(48)) x StretchingCount => AES Key(32), IV(16).
-        var bufferPosition = 0;
-        var bufferLength = SaltLength + password.Length + hashLength;
-        var buffer = new byte[bufferLength];
-        Array.Copy(salt, 0, buffer, 0, SaltLength);
-        bufferPosition += SaltLength;
-        Array.Copy(password, 0, buffer, bufferPosition, password.Length);
-        bufferPosition += password.Length;
-
-        var previousHash = Array.Empty<byte>();
-        for (var i = 0; i < StretchingCount; i++)
+        // AES
+        var aes = Aes.Create();
+        aes.Key = keyIV.Slice(0, aes.KeySize / 8).ToArray();
+        var plainLength = sizeof(ulong) + data.Length;
+        var cipherLength = aes.GetCiphertextLengthCbc(plainLength, PaddingMode.PKCS7);
+        if (keyIV.Length != ((aes.KeySize / 8) + (aes.BlockSize / 8)))
         {
-            previousHash = hash.GetHash(buffer);
-            Array.Copy(previousHash, 0, buffer, bufferPosition, hashLength);
+            throw new InvalidOperationException();
         }
 
         // Salt(8), Encrypted(n) [Checksum(8), Data(n - 8)]
-        bufferPosition = 0;
-        bufferLength = SaltLength + sizeof(ulong) + data.Length;
-        buffer = new byte[bufferLength];
+        var bufferPosition = 0;
+        var bufferLength = SaltLength + cipherLength;
+        var buffer = new byte[bufferLength];
         Array.Copy(salt, 0, buffer, 0, SaltLength);
         bufferPosition += SaltLength;
         BitConverter.TryWriteBytes(buffer.AsSpan(bufferPosition), checksum);
         bufferPosition += sizeof(ulong);
+        Array.Copy(data, 0, buffer, bufferPosition, data.Length);
 
-        // AES
-        var aes = Aes.Create();
-        aes.KeySize = 32;
-        aes.Key = previousHash;
         // var iv = new byte[16];
         // Array.Copy(previousHash, 32, iv, 0, 16);
         // aes.IV = iv;
-        aes.EncryptCbc(data, previousHash.AsSpan(32), buffer.AsSpan(bufferPosition), PaddingMode.None);
+        // var e = aes.CreateEncryptor(keyIV.Slice(0, aes.KeySize / 8).ToArray(), keyIV.Slice(aes.KeySize / 8).ToArray());
+        var written = aes.EncryptCbc(buffer.AsSpan(SaltLength, plainLength), keyIV.Slice(aes.KeySize / 8), buffer.AsSpan(SaltLength), DefaultPaddingMode);
+        Debug.Assert(written == cipherLength, "Encrypted length mismatch.");
 
         return buffer;
+    }
+
+    public static bool TryDecrypt(byte[] encrypted, string password, out Memory<byte> data) => TryDecrypt(encrypted, Encoding.UTF8.GetBytes(password), out data);
+
+    public static bool TryDecrypt(byte[] encrypted, byte[] password, out Memory<byte> data)
+    {// [MaybeNullWhen(false)]
+        data = default;
+        if (encrypted.Length < SaltLength)
+        {
+            return false;
+        }
+
+        // Hash: Sha3_384 => Key(32) + IV(16)
+        var keyIV = GetKeyIV(encrypted, password);
+
+        // AES
+        var aes = Aes.Create();
+        aes.Key = keyIV.Slice(0, aes.KeySize / 8).ToArray();
+        if (keyIV.Length != ((aes.KeySize / 8) + (aes.BlockSize / 8)))
+        {
+            throw new InvalidOperationException();
+        }
+
+        var decrypted = aes.DecryptCbc(encrypted.AsSpan(sizeof(ulong)), keyIV.Slice(aes.KeySize / 8), DefaultPaddingMode);
+        if (decrypted.Length < sizeof(ulong))
+        {
+            return false;
+        }
+
+        // Checksum: FarmHash64
+        var checksum = FarmHash.Hash64(decrypted.AsSpan(sizeof(ulong)));
+        if (BitConverter.ToUInt64(decrypted) != checksum)
+        {
+            return false;
+        }
+
+        data = decrypted.AsMemory(sizeof(ulong));
+
+        return true;
     }
 
     public static int StretchingCount
@@ -82,6 +116,31 @@ public static class PasswordEncrypt
 
             stretchingCount = value;
         }
+    }
+
+    private static Span<byte> GetKeyIV(byte[] salt, byte[] password)
+    {
+        // Hash: Sha3_384
+        var hash = new Sha3_384();
+        var hashLength = hash.HashBytes;
+
+        // SHA3-384(Salt(8) + Password(n) + Previous Hash(48)) x StretchingCount => AES Key(32), IV(16).
+        var bufferPosition = 0;
+        var bufferLength = SaltLength + password.Length + hashLength;
+        var buffer = new byte[bufferLength];
+        Array.Copy(salt, 0, buffer, 0, SaltLength);
+        bufferPosition += SaltLength;
+        Array.Copy(password, 0, buffer, bufferPosition, password.Length);
+        bufferPosition += password.Length;
+
+        var hashSpan = buffer.AsSpan(bufferPosition);
+        for (var i = 0; i < StretchingCount; i++)
+        {
+            hash.GetHash(buffer, hashSpan);
+            // Array.Copy(previousHash, 0, buffer, bufferPosition, hashLength);
+        }
+
+        return hashSpan;
     }
 
     private static int stretchingCount = DefaultStretchingCount;
---
+++ Test/PasswordEncryptTest.cs
@@ -0,0 +1,22 @@
+﻿// Copyright (c) All contributors. All rights reserved. Licensed under the MIT license.
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Arc.Crypto;
+using Xunit;
+
+namespace Test;
+
+public class PasswordEncryptTest
+{
+    [Fact]
+    public void Test1()
+    {
+        var data = new byte[] { 0, 1, 2, 3 };
+        var encrypted = PasswordEncrypt.Encrypt(data, "pass1");
+        PasswordEncrypt.TryDecrypt(encrypted, "pass1", out var decrypted);
+    }
+}
