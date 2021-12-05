521415b14822fea7ac8329f7f8878364f9b83d29
archi-Doc <archidoc422@gmail.com>
2021-12-05 21:44:53 +0900

    Add Xorshift

---
+++ Arc.Crypto/Random/Xorshift.cs
@@ -0,0 +1,77 @@
+﻿// Copyright (c) All contributors. All rights reserved. Licensed under the MIT license.
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Arc.Crypto;
+
+/// <summary>
+/// Represents a pseudo-random number generator based on Xorshift.<br/>
+/// </summary>
+public class Xorshift : RandomULong
+{
+    public static void Xor32(ref uint x)
+    {
+        if (x == 0)
+        {
+            x = 2463534242;
+            return;
+        }
+
+        x ^= x << 13;
+        x ^= x >> 17;
+        x ^= x << 5;
+    }
+
+    public static void Xor64(ref ulong x)
+    {
+        if (x == 0)
+        {
+            x = 88172645463325252UL;
+            return;
+        }
+
+        x ^= x << 13;
+        x ^= x >> 7;
+        x ^= x << 17;
+    }
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="Xorshift"/> class.<br/>
+    /// The generator is initialized with the seed 88172645463325252UL.
+    /// </summary>
+    public unsafe Xorshift()
+        : this(0)
+    {
+    }
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="Xorshift"/> class with the specified seed.
+    /// </summary>
+    /// <param name="seed">The seed.</param>
+    public unsafe Xorshift(ulong seed)
+    {
+        if (seed == 0)
+        {
+            this.seed = 88172645463325252UL;
+        }
+        else
+        {
+            this.seed = seed;
+        }
+    }
+
+    public override ulong NextULong()
+    {
+        this.seed ^= this.seed << 13;
+        this.seed ^= this.seed >> 7;
+        this.seed ^= this.seed << 17;
+
+        return this.seed;
+    }
+
+    private ulong seed;
+}
