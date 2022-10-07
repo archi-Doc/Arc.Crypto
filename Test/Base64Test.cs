f1c3c04cf8018fcef1f7ca65db0ceb0b361d37ee
archi-Doc <archidoc422@gmail.com>
2022-10-07 11:09:15 +0900

    .

---
+++ Test/Base64Test.cs
@@ -0,0 +1,37 @@
+// Copyright (c) All contributors. All rights reserved. Licensed under the MIT license.
+
+using System.Linq;
+using Arc.Crypto;
+using Xunit;
+
+#pragma warning disable SA1202 // Elements should be ordered by access
+
+namespace Test;
+
+public class Base64Test
+{
+    [Fact]
+    public void Test1()
+    {
+        var xo = new Xoshiro256StarStar(42);
+        var rv = new RandomVault(() => xo.NextUInt64(), x => xo.NextBytes(x));
+
+        for (var i = 0; i < 1000; i++)
+        {
+            var bytes = new byte[i];
+
+            for (var j = 0; j < ((i * 2) + 1); j++)
+            {
+                rv.NextBytes(bytes);
+
+                var utf = Base64.EncodeToBase64Utf8(bytes);
+                var utf2 = Base64b.FromByteArrayToUtf8(bytes);
+                utf.SequenceEqual(utf2).IsTrue();
+
+                var st = Base64.EncodeToBase64Utf8(bytes);
+                var st2 = Base64b.FromByteArrayToUtf8(bytes);
+                st.SequenceEqual(st2).IsTrue();
+            }
+        }
+    }
+}
