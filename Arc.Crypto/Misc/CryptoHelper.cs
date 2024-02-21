f6c678a43b5ff8c446ba521ee3114fca23f892c2
archi-Doc <archidoc422@gmail.com>
2024-02-21 16:25:13 +0900

    .

---
+++ Arc.Crypto/Misc/CryptoHelper.cs
@@ -0,0 +1,23 @@
+﻿// Copyright (c) All contributors. All rights reserved. Licensed under the MIT license.
+
+using System;
+using System.Diagnostics.CodeAnalysis;
+
+namespace Arc.Crypto;
+
+public static class CryptoHelper
+{
+    public static bool TryParseFromEnvironmentVariable<T>(string variable, [MaybeNullWhen(false)] out T value)
+        where T : IStringConvertible<T>
+    {
+        if (Environment.GetEnvironmentVariable(variable) is { } source)
+        {
+            return T.TryParse(source, out value);
+        }
+        else
+        {
+            value = default;
+            return false;
+        }
+    }
+}
