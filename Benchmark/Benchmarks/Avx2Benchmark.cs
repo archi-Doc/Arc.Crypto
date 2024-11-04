48c29b0b0bc453512a75945eef8ee0e45c1075ac
archi-Doc <archidoc422@gmail.com>
2024-11-04 14:46:52 +0900

    .

--- Benchmark/Benchmarks/Avx2Benchmark.cs
+++ Benchmark/Benchmarks/Avx2Benchmark.cs
@@ -295,7 +295,7 @@ public static class Avx2Methods
         h.x9 = (int)h9;
     }
 
-    public static unsafe void fe_sq2(out FieldElement h, ref FieldElement f)
+    public static unsafe void fe_sqB(out FieldElement h, ref FieldElement f)
     {// 150
         var f1 = f.x1;
         var f2 = f.x2;
@@ -322,8 +322,6 @@ public static class Avx2Methods
         long f8_19 = 19 * f8;
         long f9_38 = 38 * f9;
 
-        var f0f0 = f.x0 * (long)f.x0;
-
         /*var vec_f0_2 = Vector256.Create(f0_2);
         Span<int> stack_fm = stackalloc int[8];
         fixed (int* ptr29 = &f.x2)
@@ -333,127 +331,67 @@ public static class Avx2Methods
             vec_fm.StoreUnsafe(ref MemoryMarshal.GetReference(stack_fm));
         }*/
 
-        var f0f1_2 = f0_2 * (long)f1;
-        var f0f2_2 = f0_2 * (long)f2;
-        var f0f3_2 = f0_2 * (long)f3;
-        var f0f4_2 = f0_2 * (long)f4;
-        var f0f5_2 = f0_2 * (long)f5;
-        var f0f6_2 = f0_2 * (long)f6;
-        var f0f7_2 = f0_2 * (long)f7;
-        var f0f8_2 = f0_2 * (long)f8;
-        var f0f9_2 = f0_2 * (long)f9;
-        var f1f1_2 = f1_2 * (long)f1;
-        var f1f2_2 = f1_2 * (long)f2;
-        var f1f3_4 = f1_2 * (long)f3_2;
-        var f1f4_2 = f1_2 * (long)f4;
-        var f1f5_4 = f1_2 * (long)f5_2;
-        var f1f6_2 = f1_2 * (long)f6;
-        var f1f7_4 = f1_2 * (long)f7_2;
-        var f1f8_2 = f1_2 * (long)f8;
-        var f1f9_76 = f1_2 * f9_38;
-        var f2f2 = f2 * (long)f2;
-        var f2f3_2 = f2_2 * (long)f3;
-        var f2f4_2 = f2_2 * (long)f4;
-        var f2f5_2 = f2_2 * (long)f5;
-        var f2f6_2 = f2_2 * (long)f6;
-        var f2f7_2 = f2_2 * (long)f7;
-        var f2f8_38 = f2_2 * f8_19;
-        var f2f9_38 = f2 * f9_38;
-        var f3f3_2 = f3_2 * (long)f3;
-        var f3f4_2 = f3_2 * (long)f4;
-        var f3f5_4 = f3_2 * (long)f5_2;
-        var f3f6_2 = f3_2 * (long)f6;
-        var f3f7_76 = f3_2 * f7_38;
-        var f3f8_38 = f3_2 * f8_19;
-        var f3f9_76 = f3_2 * f9_38;
-        var f4f4 = f4 * (long)f4;
-        var f4f5_2 = f4_2 * (long)f5;
-        var f4f6_38 = f4_2 * f6_19;
-        var f4f7_38 = f4 * f7_38;
-        var f4f8_38 = f4_2 * f8_19;
-        var f4f9_38 = f4 * f9_38;
-        var f5f5_38 = f5 * f5_38;
-        var f5f6_38 = f5_2 * f6_19;
-        var f5f7_76 = f5_2 * f7_38;
-        var f5f8_38 = f5_2 * f8_19;
-        var f5f9_76 = f5_2 * f9_38;
-        var f6f6_19 = f6 * f6_19;
-        var f6f7_38 = f6 * f7_38;
-        var f6f8_38 = f6_2 * f8_19;
-        var f6f9_38 = f6 * f9_38;
-        var f7f7_38 = f7 * f7_38;
-        var f7f8_38 = f7_2 * f8_19;
-        var f7f9_76 = f7_2 * f9_38;
-        var f8f8_19 = f8 * f8_19;
-        var f8f9_38 = f8 * f9_38;
-        var f9f9_38 = f9 * f9_38;
-        var h0 = f0f0 + f1f9_76 + f2f8_38 + f3f7_76 + f4f6_38 + f5f5_38;
-        var h1 = f0f1_2 + f2f9_38 + f3f8_38 + f4f7_38 + f5f6_38;
-        var h2 = f0f2_2 + f1f1_2 + f3f9_76 + f4f8_38 + f5f7_76 + f6f6_19;
-        var h3 = f0f3_2 + f1f2_2 + f4f9_38 + f5f8_38 + f6f7_38;
-        var h4 = f0f4_2 + f1f3_4 + f2f2 + f5f9_76 + f6f8_38 + f7f7_38;
-        var h5 = f0f5_2 + f1f4_2 + f2f3_2 + f6f9_38 + f7f8_38;
-        var h6 = f0f6_2 + f1f5_4 + f2f4_2 + f3f3_2 + f7f9_76 + f8f8_19;
-        var h7 = f0f7_2 + f1f6_2 + f2f5_2 + f3f4_2 + f8f9_38;
-        var h8 = f0f8_2 + f1f7_4 + f2f6_2 + f3f5_4 + f4f4 + f9f9_38;
-        var h9 = f0f9_2 + f1f8_2 + f2f7_2 + f3f6_2 + f4f5_2;
+        var h0 = (f.x0 * (long)f.x0) + (f1_2 * f9_38) + (f2_2 * (long)f8_19) + (f3_2 * (long)f7_38) + (f4_2 * (long)f6_19) + (f5 * (long)f5_38);
+        var h1 = (f0_2 * (long)f1) + (f2 * (long)f9_38) + (f3_2 * (long)f8_19) + (f4 * (long)f7_38) + (f5_2 * (long)f6_19);
+        var h2 = (f0_2 * (long)f2) + (f1_2 * (long)f1) + (f3_2 * (long)f9_38) + (f4_2 * (long)f8_19) + (f5_2 * (long)f7_38) + (f6 * (long)f6_19);
+        var h3 = (f0_2 * (long)f3) + (f1_2 * (long)f2) + (f4 * (long)f9_38) + (f5_2 * (long)f8_19) + (f6 * (long)f7_38);
 
-        long carry0;
-        long carry1;
-        long carry2;
-        long carry3;
-        long carry4;
-        long carry5;
-        long carry6;
-        long carry7;
-        long carry8;
-        long carry9;
+        var h4 = (f0_2 * (long)f4) + (f1_2 * (long)f3_2) + (f2 * (long)f2) + (f5_2 * (long)f9_38) + (f6_2 * (long)f8_19) + (f7 * (long)f7_38);
+        var h5 = (f0_2 * (long)f5) + (f1_2 * (long)f4) + (f2_2 * (long)f3) + (f6 * (long)f9_38) + (f7_2 * (long)f8_19);
+        var h6 = (f0_2 * (long)f6) + (f1_2 * (long)f5_2) + (f2_2 * (long)f4) + (f3_2 * (long)f3) + (f7_2 * (long)f9_38) + (f8 * (long)f8_19);
+        var h7 = (f0_2 * (long)f7) + (f1_2 * (long)f6) + (f2_2 * (long)f5) + (f3_2 * (long)f4) + (f8 * (long)f9_38);
 
-        carry0 = (h0 + (long)(1 << 25)) >> 26;
+        var h8 = (f0_2 * (long)f8) + (f1_2 * (long)f7_2) + (f2_2 * (long)f6) + (f3_2 * (long)f5_2) + (f4 * (long)f4) + (f9 * (long)f9_38);
+        var h9 = (f0_2 * (long)f9) + (f1_2 * (long)f8) + (f2_2 * (long)f7) + (f3_2 * (long)f6) + (f4_2 * (long)f5);
+
+        const long r24 = 1 << 24;
+        const long r25 = 1 << 25;
+
+        long carry0 = (h0 + r25) >> 26;
         h1 += carry0;
         h0 -= carry0 << 26;
 
-        carry4 = (h4 + (long)(1 << 25)) >> 26;
+        long carry4 = (h4 + r25) >> 26;
         h5 += carry4;
         h4 -= carry4 << 26;
 
-        carry1 = (h1 + (long)(1 << 24)) >> 25;
+        long carry1 = (h1 + r24) >> 25;
         h2 += carry1;
         h1 -= carry1 << 25;
 
-        carry5 = (h5 + (long)(1 << 24)) >> 25;
+        long carry5 = (h5 + r24) >> 25;
         h6 += carry5;
         h5 -= carry5 << 25;
 
-        carry2 = (h2 + (long)(1 << 25)) >> 26;
+        long carry2 = (h2 + r25) >> 26;
         h3 += carry2;
         h2 -= carry2 << 26;
 
-        carry6 = (h6 + (long)(1 << 25)) >> 26;
+        long carry6 = (h6 + r25) >> 26;
         h7 += carry6;
         h6 -= carry6 << 26;
 
-        carry3 = (h3 + (long)(1 << 24)) >> 25;
+        long carry3 = (h3 + r24) >> 25;
         h4 += carry3;
         h3 -= carry3 << 25;
 
-        carry7 = (h7 + (long)(1 << 24)) >> 25;
+        long carry7 = (h7 + r24) >> 25;
         h8 += carry7;
         h7 -= carry7 << 25;
 
-        carry4 = (h4 + (long)(1 << 25)) >> 26;
+        carry4 = (h4 + r25) >> 26;
         h5 += carry4;
         h4 -= carry4 << 26;
 
-        carry8 = (h8 + (long)(1 << 25)) >> 26;
+        long carry8 = (h8 + r25) >> 26;
         h9 += carry8;
         h8 -= carry8 << 26;
 
-        carry9 = (h9 + (long)(1 << 24)) >> 25;
+        long carry9 = (h9 + r24) >> 25;
         h0 += carry9 * 19;
         h9 -= carry9 << 25;
 
-        carry0 = (h0 + (long)(1 << 25)) >> 26;
+        carry0 = (h0 + r25) >> 26;
         h1 += carry0;
         h0 -= carry0 << 26;
 
@@ -477,7 +415,7 @@ public class Avx2Benchmark
     {
         var fe0 = new FieldElement(1, 20, 333, -4444, 12345, 6, 7123456, 0, 1231231234, -10);
         Avx2Methods.fe_sq(out var h, ref fe0);
-        Avx2Methods.fe_sq2(out var h2, ref fe0);
+        Avx2Methods.fe_sqB(out var h2, ref fe0);
         Debug.Assert(h.Equals(h2));
     }
 
@@ -560,7 +498,7 @@ public class Avx2Benchmark
         var x4 = 0;
         for (var i = 0; i < 10; i++)
         {
-            Avx2Methods.fe_sq2(out var h, ref fe0);
+            Avx2Methods.fe_sqB(out var h, ref fe0);
             x4 += h.x4;
         }
 
