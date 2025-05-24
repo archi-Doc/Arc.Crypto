ec4d32a7d881705b0f455f41249aefa35cda994c
archi-Doc <archidoc422@gmail.com>
2025-05-24 23:50:22 +0900

    Revert "."
    This reverts commit 09cf189dc95a76de3b12182dc66aa996b83078da.

--- lib/blake3_dotnet/build-linux-arm.sh
+++ lib/blake3_dotnet/build-linux-arm.sh
@@ -4,5 +4,5 @@ BUILD="linux-arm"
 rustup target add $TARGET
 cargo build --release --target $TARGET
 mkdir -p build/$BUILD/native
-cp target/$TARGET/release/blake3_dotnet.so build/$BUILD/native
+cp target/$TARGET/release/libblake3_dotnet.so build/$BUILD/native
 arm-linux-gnueabihf-strip build/$BUILD/native/*.so
