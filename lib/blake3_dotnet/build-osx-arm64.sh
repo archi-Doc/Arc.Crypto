a66957991d035c13a6fe92e61f8c8484a023e51d
archi-Doc <archidoc422@gmail.com>
2025-05-24 23:51:55 +0900

    .

--- lib/blake3_dotnet/build-osx-arm64.sh
+++ lib/blake3_dotnet/build-osx-arm64.sh
@@ -4,4 +4,4 @@ BUILD="osx-arm64"
 rustup target add $TARGET
 cargo build --release --target $TARGET
 mkdir -p build/$BUILD/native
-cp target/$TARGET/release/libblake3_dotnet.dylib build/$BUILD/native
+cp target/$TARGET/release/libblake3_dotnet.dylib build/$BUILD/native/blake3_dotnet.so
